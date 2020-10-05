709f0e1a0aaff10b4f6245d02dd19313327e1acb
NanduVK <nanduvk999@gmail.com>
2020-10-05 18:56:05 +0530

    Add additional info in Forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -8,7 +8,7 @@ const forecast=(Latitude,Longitude,callback)=>{
         }else if(body.error){
             callback("Unable to Find Location!",undefined)
         }else{
-            callback(undefined,"Current_Weather: "+ body.current.weather_descriptions[0]+". It is currently "+body.current.temperature+" degrees out. It feels like "+body.current.feelslike+" degrees out.The Humidity is "+body.current.humidity+"."
+            callback(undefined,"Current_Weather: "+ body.current.weather_descriptions[0]+". It is currently "+body.current.temperature+" degrees out. It feels like "+body.current.feelslike+" degrees out. The Humidity is "+body.current.humidity+"."
             )
         }
     })
